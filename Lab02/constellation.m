6ca9de8c32dd41a0b40f7e094230b217e733db3c
Salar Safardoust <salar.sfd@gmail.com>
2024-09-23 00:43:42 +0330

    project files added

---
+++ Lab02/constellation.m
@@ -0,0 +1,11 @@
+function [cons, symbolEnergy] = constellation(M, modulationName)
+    switch modulationName
+        case 'pam'
+            cons = -(M-1):2:(M-1);
+        case 'psk'
+        case 'qam'
+    end
+     cons = cons/sqrt((cons*cons')/M);
+     symbolEnergy = (cons*cons')/M;
+end
+
